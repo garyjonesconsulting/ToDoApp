a3ffc938cd3ee0765c1e2080b70d24d7db8d71cd
Gary Jones <garyjonesconsulting@gmail.com>
2023-06-14 17:05:39 +1000

    Create new spring boot backend

--- backend/src/main/java/com/garyjonesconsulting/todoapp/models/Task.java
+++ backend/src/main/java/com/garyjonesconsulting/todoapp/models/Task.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity(name = "task")
 public class Task {
 
